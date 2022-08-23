c1bf76fed1ee61944a1cc345d4c52ad7529d27ee
Erika Bueno <erikab@ciandt.com>
2022-08-23 15:12:11 -0300

    Initial Commit

---
+++ UITableViewExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableViewExample
+//
+//  Created by Erika C. Matesz Bueno on 23/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
